906d831ad3b7ebf7c77d49340461fa0f76ab1baa
Dan Kozlov <kozlov-da@iszf.irk.ru>
2023-06-08 15:00:24 +0800

    Fixes from feedback: close submenu after repeated click

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -7,8 +7,18 @@ const closeDropdowns = (menu) => {
     });
 };
 
+const hasActiveSubmenu = (element) => {
+    return element.closest('.menu__item').querySelector('.menu_active');
+}
+
 [...menuLinks].forEach(element => {
     element.onclick = () => {
+        const subMenu = hasActiveSubmenu(element);
+        console.log(subMenu);
+        if (subMenu) {
+            subMenu.classList.remove('menu_active')
+            return false;
+        }
         const menu = element.closest('.menu');
         closeDropdowns(menu);
         const dropdownMenu = element.closest('.menu__item').querySelector('.menu');
