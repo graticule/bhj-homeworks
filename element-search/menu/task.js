c46925253e0199ca8cceb2a7df0166296c94a655
Dan Kozlov <kozlov-da@iszf.irk.ru>
2023-06-08 09:32:24 +0800

    Task 2 extended

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,7 +1,7 @@
 const menuLinks = document.getElementsByClassName('menu__link');
 
-const closeDropdowns = () => {
-    const dropdowns = Array.from(document.getElementsByClassName("menu_active"));
+const closeDropdowns = (menu) => {
+    const dropdowns = Array.from(menu.getElementsByClassName("menu_active"));
     dropdowns.forEach(element => {
         element.classList.remove('menu_active');
     });
@@ -9,7 +9,8 @@ const closeDropdowns = () => {
 
 [...menuLinks].forEach(element => {
     element.onclick = () => {
-        closeDropdowns();
+        const menu = element.closest('.menu');
+        closeDropdowns(menu);
         const dropdownMenu = element.closest('.menu__item').querySelector('.menu');
         if (dropdownMenu) {
             dropdownMenu.classList.add('menu_active');
