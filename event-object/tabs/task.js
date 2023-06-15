0e4cc18ca2083cd51df8e22660dd1920c5227b78
Daniil Kozlov <daniil.a.kozlov@gmail.com>
2023-06-15 18:07:50 +0800

    Task 3 (fixes)

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -3,20 +3,20 @@ const tabContents = Array.from(document.querySelectorAll('.tab__content'));
 
 const activateTab = (index) => {
     tabs[index].classList.add('tab_active');
-    tabContents[index].classList.add('tab__content_active')
+    tabContents[index].classList.add('tab__content_active');
 }
 
-const deactivateTabs = (index) => {
+const deactivateTabs = () => {
     tabs.forEach(tab => tab.classList.remove('tab_active'));
     tabContents.forEach(tabContent => tabContent.classList.remove('tab__content_active'));
 }
 
-tabs.forEach( tab => {
-    tab.onclick = () => {
-        if (!tab.classList.contains('tab_active')) {
+tabs.forEach((element) => {
+    element.onclick = () => {
+        if (!element.classList.contains('tab_active')) {
             deactivateTabs();
-            const index = tabs.indexOf(tab);
-            activateTab(index);
+            const i = tabs.indexOf(element);
+            activateTab(i);
         }
     }
 })
